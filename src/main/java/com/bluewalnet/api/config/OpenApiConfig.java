9f5ed83a15434a456f972b6a08860c89813f45a4
suhpark <suhpark@gmarket.com>
2024-03-27 10:33:00 +0900

    add swagger

---
+++ src/main/java/com/bluewalnet/api/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.bluewalnet.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+//        SecurityScheme securityScheme = new SecurityScheme()
+//                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+//                .in(SecurityScheme.In.HEADER).name("Authorization");
+//        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        Info info = new Info()
+                .title("Bluewalnut Backend Test")
+                .version("v0.0.1")
+                .description("Bluewalnut Backend Test의 API 명세서입니다.");
+
+        return new OpenAPI()
+//                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+//                .addSecurityItem(schemaRequirement)
+//                .security(Arrays.asList(schemaRequirement))
+                .info(info);
+    }
+}
